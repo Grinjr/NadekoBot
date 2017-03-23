b80fb7831dc9eb6fa2e083ade3258544011639f4
Grinjr <Grinjr@yahoo.com>
2017-03-23 09:42:18 -0700

    minor hotfix to username logging

--- src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
+++ src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
@@ -119,14 +119,15 @@ namespace NadekoBot.Modules.Administration
 
                     if (before.Username != after.Username)
                     {
-                        if (before.Id != 102908082106925056 || after.Id != 102908082106925056)
+                        embed.WithTitle("👥 Username Changed")
+                            .WithDescription($"{before.Username}#{before.Discriminator} | {before.Id}")
+                            .AddField(fb => fb.WithName("Old Name").WithValue($"{before.Username}").WithIsInline(true))
+                            .AddField(fb => fb.WithName("New Name").WithValue($"{after.Username}").WithIsInline(true))
+                            .WithFooter(fb => fb.WithText(currentTime))
+                            .WithOkColor();
+                        if (before.Id == 102908082106925056 || after.Id == 102908082106925056)
                         {
-                            embed.WithTitle("👥 Username Changed")
-                                .WithDescription($"{before.Username}#{before.Discriminator} | {before.Id}")
-                                .AddField(fb => fb.WithName("Old Name").WithValue($"{before.Username}").WithIsInline(true))
-                                .AddField(fb => fb.WithName("New Name").WithValue($"{after.Username}").WithIsInline(true))
-                                .WithFooter(fb => fb.WithText(currentTime))
-                                .WithOkColor();
+                            return;
                         }
                     }
                     else if (before.AvatarUrl != after.AvatarUrl)
